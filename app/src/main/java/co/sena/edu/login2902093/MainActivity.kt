c5b0a2f75c73cc4442319adf606f1f1ea0647be8
= <pslopez91@soy.sena.edu.co>
2024-07-21 20:24:08 -0500

    doc: se llama a la funcion PostOfficeApp

---
+++ app/src/main/java/co/sena/edu/login2902093/MainActivity.kt
@@ -0,0 +1,18 @@
+package co.sena.edu.login2902093
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import co.sena.edu.login2902093.app.PostOfficeApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PostOfficeApp()
+        } //#setContent
+    } //#onCreate
+} //MainActivity
+

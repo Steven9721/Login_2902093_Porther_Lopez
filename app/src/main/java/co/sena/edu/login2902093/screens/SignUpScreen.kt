ff98d3789ca6d5dfa66d5ca1d05e2f27dfc2c541
= <pslopez91@soy.sena.edu.co>
2024-07-21 20:25:36 -0500

    Refactor: Se añadio un Column para la insercion de la funcion NormalTextComponent y la nueva funcion HeadingTextComponent

---
+++ app/src/main/java/co/sena/edu/login2902093/screens/SignUpScreen.kt
@@ -0,0 +1,41 @@
+package co.sena.edu.login2902093.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.sena.edu.login2902093.R
+import co.sena.edu.login2902093.components.HeadingTextComponent
+import co.sena.edu.login2902093.components.NormalTextComponent
+
+@Composable
+fun SignUpScreen() {
+    Surface(
+        color = Color.White,
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxSize()
+            .padding(28.dp)
+
+    ) {
+       Column (modifier= Modifier.fillMaxSize()){
+           NormalTextComponent(value = stringResource(id = R.string.hello))
+           HeadingTextComponent(value = stringResource(id = R.string.create_account))
+       }
+
+    } //Surface
+} //SignUpScreen
+
+
+@Preview
+@Composable
+fun DefaultSignUpScreen() {
+    SignUpScreen()
+}
